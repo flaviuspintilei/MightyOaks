e20980672c6c1bb3b83c5f91e93fa8a99a3adfb9
Pintilei Flavius <flaviuspintilei97@gmail.com>
2021-10-05 18:57:53 +0300

    JS file uploaded

---
+++ js/navbar.js
@@ -0,0 +1,39 @@
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector(".nav-bar");
+    console.log(navbar);
+    navbar.classList.toggle('sticky', window.scrollY > 0);
+})
+
+var aboutHeader = document.getElementById("aboutDropdown");
+
+var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+
+aboutHeader.addEventListener("mouseover", () => {
+    dropdownContent.classList.add("display-block");
+});
+
+aboutHeader.addEventListener("mouseout", () => {
+    dropdownContent.classList.remove("display-block");
+});
+
+dropdownContent.addEventListener("mouseover", () => {
+    dropdownContent.classList.add("display-block");
+})
+
+dropdownContent.addEventListener("mouseout", () => {
+    dropdownContent.classList.remove("display-block");
+});
+
+document.getElementById("historyMenu").addEventListener('click', historyFunction);
+
+function historyFunction() {
+    document.getElementById("jumpToTheContent").scrollIntoView({behavior: 'smooth'});
+}
+
+function membersFunction() {
+    window.location.hash = "jumpToTheMembers";
+}
+
+
+
+
