3a8f5fd7fc4b2863524e3d33da69581878f5a67c
Pintilei Flavius <flaviuspintilei97@gmail.com>
2021-10-11 11:44:21 +0300

    Website finished

--- js/albums.js
+++ js/albums.js
@@ -5,7 +5,6 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-    console.log(this);
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
